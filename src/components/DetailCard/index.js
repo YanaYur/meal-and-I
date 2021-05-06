ab81e0c2dbdb417e4c5a2c2ea9af4b737fc18791
Yana Yur <yana.yuryeva@gmail.com>
2021-05-06 19:30:00 +0200

    Updated Detail Component

--- src/components/DetailCard/index.js
+++ src/components/DetailCard/index.js
@@ -1,14 +1,39 @@
+import { AiOutlineLeft, AiOutlineStar} from 'react-icons/ai'
+
 import './styles.scss';
 
-const DetailCard = () => {
-
+function loading() {
     return (
-        <div className="details">
-            <div className="details-card">
-                <div className="details-card__container">
-                    </div>
-            </div>
+        <div>Loading...</div>
+    )
+}
+
+function detailMeal(meal) {
+    return (
+        <div key={meal.idMeal} className="detail-meal">
+             <button type="button" className="back" value="click" ><AiOutlineLeft /></button>
+             <button type="button" className="back" value="click" ><AiOutlineStar /></button>
+            <img className="detail-meal__image" src={meal.strMealThumb} alt={meal.strMeal}></img>
+            <div className="detail-meal__name">{meal.strMeal}</div>
+            <div className="detail-meal__ingredients">{meal.strMeasure1} {meal.strIngredient1}</div>
+            <div className="detail-meal__ingredients">{meal.strMeasure2} {meal.strIngredient2}</div>
+            <div className="detail-meal__ingredients">{meal.strMeasure3} {meal.strIngredient3}</div>
+            <div className="detail-meal__ingredients">{meal.strMeasure4} {meal.strIngredient4}</div>
+            <div className="detail-meal__ingredients">{meal.strMeasure5} {meal.strIngredient5}</div>
+            <div className="detail-meal__instructions">{meal.strInstructions}</div>
+        </div>);
+
+};
+
+
+const DetailCard = ({ data }) => {
+   
+    return (
+        <div className="detail-info">
+
+            {data ? detailMeal(data) : loading()}
+
         </div>
     );
-}
+};
 export default DetailCard;
