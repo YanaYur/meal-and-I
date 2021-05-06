24a91968162c9d2eb5f112d4a12616026ea08e3d
Yana Yur <yana.yuryeva@gmail.com>
2021-05-06 11:45:00 +0200

    Added functionality to Logic

--- mealandi/src/Logic/meals.js
+++ mealandi/src/Logic/meals.js
@@ -8,15 +8,28 @@ async function getMainIngredients() {
 }
 
 async function getTrendyMeals() {
-    const { data } = await axios(`${process.env.REACT_APP_API_URL_BASE}search.php?s=Arrabiata`);
-    return data;
+    const { data } = await axios(`${process.env.REACT_APP_API_URL_BASE}search.php?f=b`);
+    const newData=data.meals.slice(0, 5);
+    return newData;
 }
 
 async function getRandomMeal() {
     const { data } = await axios(`${process.env.REACT_APP_API_URL_BASE}random.php`);
+
+    return data;
+}
+
+async function getMealByIngredient(selectedIngredient){
+    debugger
+    const { data } = await axios(`${process.env.REACT_APP_API_URL_BASE}filter.php?i=${selectedIngredient}`);
+    const newData=data.meals.slice(0, 5);
+    return newData;
+}
+
+async function getMealById(id){
+    const { data } = await axios(`${process.env.REACT_APP_API_URL_BASE}lookup.php?i${id}`);
     return data;
 }
 
 
-
-export default {getMainIngredients, getRandomMeal, getTrendyMeals};
+export default {getMainIngredients, getRandomMeal, getTrendyMeals, getMealByIngredient, getMealById};
