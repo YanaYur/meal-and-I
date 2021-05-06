6520ba0efead46a34df4e60e2cf646de14d9a4bb
Yana Yur <yana.yuryeva@gmail.com>
2021-05-06 12:05:35 +0200

    Updated app.js

--- mealandi/src/App.js
+++ mealandi/src/App.js
@@ -4,6 +4,10 @@ import { AuthProvider } from "./context/AuthContext";
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Login from './components/Login/Login'
 import Home from './containers/Home'
+import Search from './containers/Search';
+import Detail from './containers/Detail';
+
+import './assets/styles/config.scss';
 
 function App() {
   return(
@@ -13,11 +17,17 @@ function App() {
         <Route exact path ="/">
           <Home/>
         </Route>
+        <Route path="/login">
+          <Login/>
+        </Route>
         <Route path ="/signup">
           <Signup/>
         </Route>
-        <Route path="/login">
-          <Login/>
+        <Route path="/search">
+          <Search/>
+        </Route>
+        <Route path="/detail">
+          <Detail/>
         </Route>
       </Switch>
     </AuthProvider>
