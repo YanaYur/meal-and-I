fb8b22a07e13f93a1f1625a6e20faa608f6e7543
Yana Yur <yana.yuryeva@gmail.com>
2021-05-06 11:49:00 +0200

    Updated Random component

--- mealandi/src/components/Random/index.js
+++ mealandi/src/components/Random/index.js
@@ -1,26 +1,36 @@
-import React from 'react';
-
-
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Title from '../Title/index'
-import {ImMagicWand} from 'react-icons/im'
+import { ImMagicWand } from 'react-icons/im'
 import './styles.scss';
 
-// const coverRandom = () => {
-//     return <div key="1" className="random-cover">
-//         <img src={random} alt='cover'></img>
-//     </div>
-// }
+function loading() {
+    return (
+        <div>Loading...</div>
+    )
+}
 
-const Random = () => {
+const Random = ({ data, handleUpdateRandom }) => {
+   
+    
+
     return (
         <>
-            <Title name="Generate Random Meal" />
+            <Title name="Generated Random Meal" />
             <div className="random-meal-card">
-                <div className="random-meal-card__cover">
-                  
-                </div>
+                {data ?
+                    <div className="random-meal-card__cover">
+                        <Link to={{
+                            pathname: "/detail",
+                            search: `?meal=${data.meals[0].idMeal}`,
+                        }}>
+                            <img src={data.meals[0].strMealThumb} alt={data.meals[0].strMeal}></img>
+                            <p>{data.meals[0].strMeal}</p>
+                        </Link>
+                    </div>
+                    : loading()}
                 <div className="random-meal-card__button">
-                    <button type="button" value="click"><ImMagicWand/></button>
+                    <button type="button" value="click" onClick={handleUpdateRandom}><ImMagicWand /></button>
                 </div>
             </div>
         </>
