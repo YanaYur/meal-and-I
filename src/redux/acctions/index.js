bcfc2f0fc92488007d5457896dc4fcfea463d5e2
Yana Yur <yana.yuryeva@gmail.com>
2021-05-11 22:04:07 +0200

    Added Redux ingredients

--- src/redux/acctions/index.js
+++ src/redux/acctions/index.js
@@ -1,14 +1,15 @@
-import { UPDATE_SELECTED_INGREDIENTS } from '../types';
+import { UPDATE_SELECTED_INGREDIENTS, SET_FAVOURITE } from '../types';
 
-// export function setUser(name) {
-//     return { type: "SET_USER", payload: name };
-// }
 
 export function updateSelectedIngredients(ingredients) {
     return { type: UPDATE_SELECTED_INGREDIENTS, payload: ingredients };
 }
 
 
-// export function setFavorite(meal){
-//     return {type:"SET_FAVORITE", payload:meal}
-// }
+export function setFavourite(favorites){
+    return {type:SET_FAVOURITE, payload:favorites}
+}
+
+export function setUser(name) {
+    return { type: "SET_USER", payload: name };
+}
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,18 +1,21 @@
-import { UPDATE_SELECTED_INGREDIENTS } from '../types';
+import { UPDATE_SELECTED_INGREDIENTS, SET_FAVOURITE, SET_USER } from '../types';
 
-const defaultMeals = {
+const defaultInfo = {
     selectedIngredients: [],
-    // favorites:[]
+    favorites:['52772','52773'],
+    user:undefined
 };
 
 
-export default function mealsReducer(state = defaultMeals, action) {
+export default function mealsReducer(state = defaultInfo, action) {
     switch (action.type) {
         case UPDATE_SELECTED_INGREDIENTS:
             return { ...state, selectedIngredients: [...action.payload] }
-        // case "FAVORITES":
-        //         return {...state, setFavorite: action.payload}
+        case SET_FAVOURITE:
+                return {...state, favorites: action.payload}
+        case SET_USER:
+            return {...state, user: action.payload}
         default:
-            return defaultMeals;
+            return defaultInfo;
     }
 }
