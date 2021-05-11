1b20bac629c241db323b5a7c3f6905fda9d8aee2
Yana Yur <yana.yuryeva@gmail.com>
2021-05-11 22:15:11 +0200

    Updated context and firebase configuration

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
 import React from 'react';
 import Signup from "./components/Signup/SignUp";
 import { AuthProvider } from "./context/AuthContext";
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Login from './components/Login/Login'
 import Home from './containers/Home'
 import Search from './containers/Search';
 import Detail from './containers/Detail';
+import Favorites from './containers/Favorites';
+import Profile from './containers/Profile';
+import PrivateRoute from './HOC/PrivateRoute'
 
 import './assets/styles/config.scss';
 
 function App() {
-  return(
-  <Router>
-    <AuthProvider>
-      <Switch>
-        <Route exact path ="/">
-          <Home/>
-        </Route>
-        <Route path="/login">
-          <Login/>
-        </Route>
-        <Route path ="/signup">
-          <Signup/>
-        </Route>
-        <Route path="/search">
-          <Search/>
-        </Route>
-        <Route path="/detail">
-          <Detail/>
-        </Route>
-      </Switch>
-    </AuthProvider>
+  return (
+    <Router>
+      <AuthProvider>
+        <Switch>
+          
+          <Route exact path="/" component={Login} />
+          <Route path="/signup" component={Signup} />
 
-  </Router>
+          <PrivateRoute path="/home" component={Home} />
+          <PrivateRoute path="/search" component={Search} />
+          <PrivateRoute path="/detail" component={Detail} />
+          <PrivateRoute path="/favorites" component={Favorites} />
+          <PrivateRoute path="/profile" component={Profile} />
+
+        </Switch>
+      </AuthProvider>
+
+    </Router>
   )
-  
+
 }
 
 export default App;
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { auth } from '../firebase';
+import { setUser } from '../redux/acctions'
+import { userSelector } from '../redux/selectors/selectors'
+import { useDispatch, useSelector } from 'react-redux';
 
 const AuthContext = React.createContext();
 
@@ -8,27 +11,38 @@ export function useAuth() {
 }
 
 export function AuthProvider({ children }) {
-    const [currentUser, setCurrentUser] = useState();
+    // const [currentUser, setCurrentUser] = useState("");
     const [loading, setLoading] = useState(true);
+    const dispatch = useDispatch();
+    const currentUser = useSelector(userSelector);
 
     const value = {
         currentUser,
+        login,
         signup,
-        login
+        logout,
     };
 
-    function signup (email, password) {
+    function signup(email, password) {
         return auth.createUserWithEmailAndPassword(email, password);
+
+
     }
 
-    function login (email, password) {
+    function login(email, password) {
         return auth.signInWithEmailAndPassword(email, password);
+    }
+
+    function logout() {
+        return auth.signOut();
     }
 
     useEffect(() => {
         const newUser = auth.onAuthStateChanged(user => {
-            setCurrentUser(user);
+            const { uid, email } = user;
+            dispatch(setUser({ uid, email }));
             setLoading(false);
+
         })
         return newUser;
     }, []);
